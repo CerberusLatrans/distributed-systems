5053ca216c436916edb0a0981ad87c9487be6a6a
Garamon <ollietoh@gmail.com>
2024-10-22 19:09:35 -0400

    working non tls

--- project_1/src/guess.rs
+++ project_1/src/guess.rs
@@ -2,40 +2,48 @@ use std::collections::HashMap;
 
 use crate::message::Score;
 
-pub fn make_guess(history: Vec<Score>, mut word_list: &Vec<&str>) -> String {
-    let last_score = history.last().unwrap();
-    let word: Vec<char> = last_score.word.chars().collect();
-    let marks = last_score.marks;
-    let mut contained = Vec::new();
-    let mut correct = HashMap::new();
-    let mut absent = Vec::new();
-    for i in 1..6 {
-        match marks[i] {
-            0 => absent.push(word[i]),
-            1 => contained.push(word[i]),
-            2 => {correct.insert(i, word[i]);},
+pub fn filter_words(history: Vec<Score>, mut word_list: Vec<&str>) -> Vec<&str> {
+    let last_history = history.last();
+    if let Some(last_score) = last_history {
+        let word: Vec<char> = last_score.word.chars().collect();
+        let marks = last_score.marks.clone();
+        let mut contained = Vec::new();
+        let mut correct = HashMap::new();
+        let mut absent = Vec::new();
+        for i in 0..word.len() {
+            match marks[i] {
+                2 => {correct.insert(i, word[i]);},
+                1 => contained.push(word[i]),
+                0 => absent.push(word[i]),
+                _ => panic!("Unknown mark: {}", marks[i]),
+            }
         }
+        let correct_letters: Vec<char> = correct.values().cloned().collect();
+        absent.retain(|s| !correct_letters.contains(s) && !contained.contains(s));
+        word_list.retain(
+            |s| -> bool {
+                if s==&last_score.word {
+                    return false
+                }
+                let word_chars: Vec<char> = s.chars().collect();
+                for c in &absent {
+                    if word_chars.contains(c) {
+                        return false
+                    }
+                }
+                for c in &contained {
+                    if !word_chars.contains(c) {
+                        return false
+                    }
+                }
+                for (k,v) in correct.iter() {
+                    if word_chars[*k]!= *v {
+                        return false
+                    }
+                }
+                true
+            }
+        );
     }
-    let is_valid = |s: &str| -> bool {
-        let word: Vec<char> = s.chars().collect();
-        for c in absent {
-            if word.contains(&c) {
-                return false
-            }
-        }
-        for c in contained {
-            if !word.contains(&c) {
-                return false
-            }
-        }
-        for (k,v) in correct.iter() {
-            if word[*k]!= *v {
-                return false
-            }
-        }
-        true
-    };
-    word_list = word_list.into_iter().filter(is_valid).collect();
-    word_list[0].to_owned()
-    //word_list[history.len()].to_owned()
+    word_list
 }
--- project_1/src/main.rs
+++ project_1/src/main.rs
@@ -1,24 +1,20 @@
-use std::io::{Read, Write};
+use std::io::{stdout, BufRead, BufReader, Read, Write};
+use std::sync::Arc;
 use std::{fs, str};
 use std::path::PathBuf;
 use std::net::TcpStream;
 use clap::Parser;
+use rustls::pki_types::ServerName;
+use native_tls::TlsConnector;
 use serde_json::Value;
-//use rustls::RootCertStore;
+use rustls::{ClientConnection, RootCertStore, Stream};
 
 mod message;
-use message::{
-    ByeMessage,
-    ErrorMessage,
-    GuessMessage,
-    HelloMessage,
-    RetryMessage,
-    Score,
-    StartMessage
+use message::{ ByeMessage, ErrorMessage, GuessMessage, HelloMessage, RetryMessage, Score, StartMessage
 };
 
 mod guess;
-use guess::make_guess;
+use guess::filter_words;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -37,7 +33,7 @@ const DEFAULT_PORT: u16 = 27993;
 const DEFAULT_TLS_PORT: u16 = 27994;
 const WORD_LIST_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/", "src", "/", "words.txt");
 
-fn parse_args() -> (String, u16, String) {
+fn parse_args() -> (String, u16, String, bool) {
     let args = Args::parse();
     let port = match args.port {
         Some(p) => p,
@@ -45,16 +41,53 @@ fn parse_args() -> (String, u16, String) {
     };
     let hostname = args.hostname;
     let username = args.username;
-    (hostname, port, username)
+    (hostname, port, username, args.tls)
 }
 
-fn handshake(hostname: String, port: u16) -> TcpStream {
+fn handshake(hostname: String, port: u16, tls: bool) -> TcpStream {
     if let Ok(s) = TcpStream::connect((hostname.clone(), port)) {
-        s
+        if tls {
+            //let connector = TlsConnector::new().unwrap();
+            //let mut stream = connector.connect(&hostname, s).unwrap();
+            //stream.write_all(b"GET / HTTP/1.0\r\n\r\n").unwrap();
+            //stream
+            s
+        } else {
+            s
+        }
     } else {
         panic!("Failed to connect to server {} on port {}", hostname, port);
     }
 }
+
+//fn enable_tls(host: String, mut sock: &TcpStream) -> Stream<'_,ClientConnection, &TcpStream>  {
+//    let root_store = RootCertStore {
+//        roots: webpki_roots::TLS_SERVER_ROOTS.into(),
+//    };
+//    let mut config = rustls::ClientConfig::builder()
+//        .with_root_certificates(root_store)
+//        .with_no_client_auth();
+//
+//    // Allow using SSLKEYLOGFILE.
+//    config.key_log = Arc::new(rustls::KeyLogFile::new());
+//
+//    let server_name: ServerName = host.clone().try_into().unwrap();
+//    let mut conn = rustls::ClientConnection::new(Arc::new(config), server_name).unwrap();
+//    let mut tls = rustls::Stream::new(&mut conn, &mut sock);
+//    tls.write_all(
+//        format!(
+//        concat!(
+//            "GET / HTTP/1.1\r\n",
+//            "Host: {}\r\n",
+//            "Connection: close\r\n",
+//            "Accept-Encoding: identity\r\n",
+//            "\r\n"
+//        ), host)
+//        .as_bytes(),
+//    )
+//    .unwrap();
+//    tls
+//}
 
 // write hello message to stream
 fn greet_server(username: String, mut stream: &TcpStream) {
@@ -64,8 +97,10 @@ fn greet_server(username: String, mut stream: &TcpStream) {
     let _ = stream.write(&hello_json.as_bytes());
 }
 
-fn send_guess(mut stream: &TcpStream, id: String, history: Vec<Score>, mut word_list: &Vec<&str>) {
-    let guess_word= make_guess(history, &word_list);
+fn send_guess(
+    mut stream: &TcpStream,
+    id: String,
+    guess_word: String) {
     let guess = GuessMessage::new(guess_word, id.to_owned());
     let mut guess_json = serde_json::to_string(&guess).unwrap();
     guess_json.push_str("\n");
@@ -82,65 +117,45 @@ fn get_msg_type(server_msg: &Value) -> &str {
     }
 }
 
-fn get_server_msg(buffer: &mut [u8], n: usize) -> Value {
-    //let server_msg_pre = serde_json::from_slice(trunc_buffer);
+fn get_server_msg(buffer: &mut [u8]) -> Value {
     let mut msg_str = str::from_utf8(buffer).unwrap();
-    println!("{}", msg_str);
-    msg_str = msg_str.trim();
-    println!("{}", msg_str);
-    let server_msg_pre = serde_json::from_str(&msg_str[..msg_str.len()-1]);
+    msg_str = msg_str.trim_matches(char::from(0)).trim();
+    let server_msg_pre = serde_json::from_str(&msg_str);
     let server_msg: Value = match server_msg_pre {
-        Ok(x) => {
-            println!("Read {} bytes", n);
-            println!(
-                "GOOD: {}\nFULL: {}",
-                str::from_utf8(&buffer[..n]).unwrap(),
-                str::from_utf8(&buffer).unwrap()
-            );
-            x
-        },
-        Err(e) => {
-            println!("{:?}, read {} bytes", e, n);
-            //println!("LAST BYTE: {:?}", &client_buffer[n-3..n+3]);
-            panic!(
-                "ERROR: {}",
-                msg_str);
-        }
+        Ok(x) => x,
+        Err(e) => {panic!("ERROR: {} trying to parse {}", e, msg_str);}
     };
-    println!("LAST BYTE: {}", &buffer[n]);
     buffer.fill(0);
-    //client_buffer.clear();
     server_msg
 }
 
 fn main() {
-    let (hostname, port, username) = parse_args();
+    let (hostname, port, username, tls) = parse_args();
     //Connect to the server
-    let mut stream = handshake(hostname, port);
+    let stream = handshake(hostname, port, tls);
     greet_server(username, &stream);
 
-    let mut client_buffer = [0u8; 1024*128];
-    //let mut client_buffer = Vec::new();
+    //let mut client_buffer = [0u8; BUFFER_SIZE];
+    let mut client_buffer = Vec::new();
     let mut id: Option<String> = None;
     
     // read in words.txt into word list
     let word_list_text = fs::read_to_string(PathBuf::from(WORD_LIST_PATH)).unwrap();
-    let word_list = word_list_text.split_ascii_whitespace().collect();
+    let mut word_list: Vec<&str> = word_list_text.split_ascii_whitespace().collect();
+    let mut packet = BufReader::new(&stream);
     loop {
-        match stream.read(&mut client_buffer) { //ERROR: n is cut off message: the whole message has id which from index n to n+j is missing some characters
+        match packet.read_until(b'\n', &mut client_buffer) {
             Ok(n) => {
-                let server_msg = get_server_msg(&mut client_buffer, n);
+                let server_msg = get_server_msg(&mut client_buffer);
                 let mut history: Vec<Score> = Vec::new();
                 let server_msg_type = get_msg_type(&server_msg);
                 match server_msg_type {
                     "start" => {
                         let start: StartMessage = serde_json::from_value(server_msg).unwrap();
-                        println!("START");
                         id = Some(start.id);
                     },
                     "retry" => {
                         let retry: RetryMessage = serde_json::from_value(server_msg).unwrap();
-                        println!("RETRY");
                         history = retry.guesses;
                     },
                     "error" => {
@@ -158,7 +173,12 @@ fn main() {
                 }
                 // write a guess if the id has been given
                 if let Some(identification) = &id {
-                    send_guess(&stream, identification.to_owned(), history, &word_list);
+                    word_list = filter_words(history, word_list);
+                    if word_list.len()>0 {
+                        send_guess(&stream, identification.to_owned(), String::from(word_list[0]));
+                    } else {
+                        panic!("Guessed all possible words!");
+                    }             
                 }
             },
             Err(error) => {
